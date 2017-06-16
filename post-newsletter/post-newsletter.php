ed3d37f62beb87b88622bc2ec5f61c8f07906aa8
Alexandru Berce <alexandru.berce@gmail.com>
2017-06-17 01:14:08 +0300

    Adding plugin bootstrap file

---
+++ post-newsletter/post-newsletter.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Post Newsletter
+ * Plugin URI:        http://captainform.com
+ * Description:       Notify your visitors every time you create a new post
+ * Version:           1.0.0
+ * Author:            captainform
+ * Author URI:        https://profiles.wordpress.org/captainform
+ * License:           MIT
+ * Text Domain:       post-newsletter
+ * Domain Path:       /languages
+ */
